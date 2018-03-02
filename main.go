52b5508a74726a90057d211c2e4d7557b046c846
Julien BONACHERA <jbonachera@xebia.fr>
2018-03-02 10:01:00 +0100

    use localhost as default broker

--- main.go
+++ main.go
@@ -131,6 +131,7 @@ func main() {
 	viper.SetConfigName("config")             // name of config file (without extension)
 	viper.AddConfigPath("$HOME/.mqtt-client") // call multiple times to add many search paths
 	viper.AddConfigPath(".")                  // optionally look for config in the working directory
+	viper.SetDefault("mqtt.broker", "tcp://localhost:1883")
 	err := viper.ReadInConfig()               // Find and read the config file
 	if err != nil { // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
