fce93542c2b1fc0bed9f958759a67c252ef88b78
Julien BONACHERA <julien.bonachera@fortuneo.com>
2019-12-17 15:52:50 +0100

    support SNI

--- mqtt.go
+++ mqtt.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"net"
+	"net/url"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -12,12 +15,30 @@ import (
 )
 
 func client(d MQTT.OnConnectHandler, l MQTT.ConnectionLostHandler) (MQTT.Client, error) {
-	opts := MQTT.NewClientOptions().AddBroker(viper.GetString("mqtt.broker"))
+	broker := viper.GetString("mqtt.broker")
+	opts := MQTT.NewClientOptions().AddBroker(broker)
 	opts.Username = viper.GetString("mqtt.username")
 	opts.Password = viper.GetString("mqtt.password")
 	opts.SetClientID(fmt.Sprintf("mqtt-cli-%s", time.Now().Format("15:04:05")))
 	opts.OnConnect = d
 	opts.OnConnectionLost = l
+	brokerURL, err := url.Parse(broker)
+	if err != nil {
+		panic(err)
+	}
+	if brokerURL.Scheme == "tls" {
+		host, _, _ := net.SplitHostPort(brokerURL.Host)
+		opts.TLSConfig = &tls.Config{
+			MinVersion:               tls.VersionTLS12,
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			},
+			ServerName: host,
+		}
+	}
 	c := MQTT.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		return nil, token.Error()
@@ -31,6 +52,6 @@ func now() string {
 
 func connLostHandler(app *cobra.Command) MQTT.ConnectionLostHandler {
 	return func(client MQTT.Client, e error) {
-		fmt.Fprintf(app.OutOrStderr(), "%s connection to broker lost - reconnecting..\n", color.GreenString(now()))
+		fmt.Fprintf(app.OutOrStderr(), "%s connection to broker lost (%v) - reconnecting..\n", e, color.GreenString(now()))
 	}
 }
